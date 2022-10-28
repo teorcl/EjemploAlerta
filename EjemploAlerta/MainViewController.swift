b22e5b04ee96dd8848e0e0d6ab025e31335351f1
Teodoro <teo.calle.lara@gmail.com>
2022-10-28 16:27:53 -0500

    Se refactoriza el código con una función para mensajes y se creaN CONSTANTES para los mensajes

--- EjemploAlerta/MainViewController.swift
+++ EjemploAlerta/MainViewController.swift
@@ -8,19 +8,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    struct Const{
+        static let messageOk = "Accion ok presionada"
+        static let messageCancel = "Accion cancelar presionada"
+        static let messageDestructive = "Accion destruir presionada"
     }
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     @IBAction func onActionTapped() {
         //Aqui vamos a crear la alerta
         let alert = UIAlertController(title: "Titulo de la alerta", message: "Cuerpo de la alerta", preferredStyle: .alert)
         //let okAction = UIAlertAction(title: "OK diferente", style: .default) //Action para cerrar la alerta
         
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            print("Acción presionada")
+            //self.resultLabel.text = "Accion ok presionada"
+            /*====self.resultLabel.isHidden = false====*/
+            self.showResultMessage(message: Const.messageOk)
         } //Action para cerrar la alerta
         
         //let cancelAction = UIAlertAction(title: "Cancelar Mio", style: .default)
@@ -29,12 +34,21 @@ class MainViewController: UIViewController {
         alert.addAction(okAction)
         //alert.addAction(cancelAction)
         
+        let destructiveAction = UIAlertAction(title: "Destruir", style: .destructive) { _ in
+            //self.resultLabel.text = "Accion destruir presionada"
+            /*====self.resultLabel.isHidden = false====*/
+            self.showResultMessage(message: Const.messageDestructive)
+        }
+        alert.addAction(destructiveAction)
+        
        /* let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
             print("Acción cancel presionada")
         } *///Estilo cancel me lleva el boton a la izquierda y le pone el texto en negrita
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { _ in
-            print("Acción cancel presionada")
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            //print("Acción cancel presionada")
+            /*====self.resultLabel.isHidden = false====*/
+            self.showResultMessage(message: Const.messageCancel)
         } //Esta no tiene prioridad en el orden del boton, pero me pone el texto en color rojo
         
         alert.addAction(cancelAction)
@@ -42,5 +56,15 @@ class MainViewController: UIViewController {
         present(alert, animated: true) //Presentación de la alerta
     }
     
+    //==================================================
+    /**
+     Las lineas 21, 32, 42 son código repetitivo en su lugar es mejor hacer una función para ello
+     */
+    func showResultMessage(message: String){
+        resultLabel.text = message
+        resultLabel.isHidden = false
+    }
+    
 }
+
 
