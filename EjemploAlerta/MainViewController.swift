605364a52c8c441adbe26f34d65aa25847ea36c1
Teodoro <teo.calle.lara@gmail.com>
2022-10-28 15:23:31 -0500

    Se aplica el estilo cancel a la acción de cancelar

--- EjemploAlerta/MainViewController.swift
+++ EjemploAlerta/MainViewController.swift
@@ -18,11 +18,23 @@ class MainViewController: UIViewController {
         //Aqui vamos a crear la alerta
         let alert = UIAlertController(title: "Titulo de la alerta", message: "Cuerpo de la alerta", preferredStyle: .alert)
         //let okAction = UIAlertAction(title: "OK diferente", style: .default) //Action para cerrar la alerta
-        let okAction = UIAlertAction(title: "OK", style: .default) //Action para cerrar la alerta
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            print("Acción presionada")
+        } //Action para cerrar la alerta
+        
         //let cancelAction = UIAlertAction(title: "Cancelar Mio", style: .default)
+        
         //Ahora le agregamos la acción a la alerta nótese el metodo addAction
         alert.addAction(okAction)
         //alert.addAction(cancelAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("Acción cancel presionada")
+        } //Estilo cancel me lleva el boton a la izquierda y le pone el texto en negrita
+        
+        alert.addAction(cancelAction)
+        
         present(alert, animated: true) //Presentación de la alerta
     }
     
