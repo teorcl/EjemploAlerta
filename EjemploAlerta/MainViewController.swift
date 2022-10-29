c884473f4ed536fab2790728414122c3d1cdd230
Teodoro <teo.calle.lara@gmail.com>
2022-10-29 01:04:19 -0500

    Se identifican varios textField y se refactoriza el código

--- EjemploAlerta/MainViewController.swift
+++ EjemploAlerta/MainViewController.swift
@@ -15,56 +15,47 @@ class MainViewController: UIViewController {
         static let messageDestructive = "Accion destruir presionada"
     }
     
+    private struct Tags{
+        static let nameTextFieldTag = 6
+    }
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func onActionTapped() {
         //Aqui vamos a crear la alerta
         let alert = UIAlertController(title: "Titulo de la alerta", message: "Cuerpo de la alerta", preferredStyle: .alert)
-        //let okAction = UIAlertAction(title: "OK diferente", style: .default) //Action para cerrar la alerta
-        
+                
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            //self.resultLabel.text = "Accion ok presionada"
-            /*====self.resultLabel.isHidden = false====*/
+                        
+                guard  let textFields = alert.textFields else{
+                    return
+                }
             
-            //self.showResultMessage(message: Const.messageOk)
-            if let textFields = alert.textFields{
-                //cuerpo del optional binding
-                let nameTextField = textFields[0]
+                guard let nameTextField = textFields.first(where: { textFieldUnderAnalysis in
+                    return textFieldUnderAnalysis.tag == Tags.nameTextFieldTag
+                }) else{
+                        return
+                    }
+                
                 let name = nameTextField.text ?? ""
                 self.showResultMessage(message: "Su nombre completo es: \(name)")
+                
             }
             
             
-            
-        } //Action para cerrar la alerta
-        
-        //let cancelAction = UIAlertAction(title: "Cancelar Mio", style: .default)
-        
-        //Ahora le agregamos la acción a la alerta nótese el metodo addAction
         alert.addAction(okAction)
-        //alert.addAction(cancelAction)
+      
         
-      /*----  let destructiveAction = UIAlertAction(title: "Destruir", style: .destructive) { _ in
-            //self.resultLabel.text = "Accion destruir presionada"
-            /*====self.resultLabel.isHidden = false====*/
-            self.showResultMessage(message: Const.messageDestructive)
-        }
-        alert.addAction(destructiveAction)
-        
-       /* let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
-            print("Acción cancel presionada")
-        } *///Estilo cancel me lleva el boton a la izquierda y le pone el texto en negrita
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
-            //print("Acción cancel presionada")
-            /*====self.resultLabel.isHidden = false====*/
-            self.showResultMessage(message: Const.messageCancel)
-        } //Esta no tiene prioridad en el orden del boton, pero me pone el texto en color rojo
-        
-        alert.addAction(cancelAction)  ----*/
-        
+        // TextField 1
         alert.addTextField { textFieldToBeConfigured in
-            textFieldToBeConfigured.placeholder = "Su nombre completo"
+            textFieldToBeConfigured.placeholder = "Su nombre "
+            textFieldToBeConfigured.tag = Tags.nameTextFieldTag
+        }
+        
+        // TextField 2
+        alert.addTextField { textFieldToBeConfigured in
+            textFieldToBeConfigured.placeholder = "Su apellido"
+            textFieldToBeConfigured.tag = 9
         }
         
         present(alert, animated: true) //Presentación de la alerta
